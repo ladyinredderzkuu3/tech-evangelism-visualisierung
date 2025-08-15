e28bfcefa3e6c19eddcbc6092d25500344ae3f82
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-08-15 05:36:43 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TechnologyVisualizer from './TechnologyVisualizer';
+
+function App() {
+  return (
+    <div>
+      <h1>Technologie Visualisierungstool</h1>
+      <TechnologyVisualizer />
+    </div>
+  );
+}
+
+export default App;
