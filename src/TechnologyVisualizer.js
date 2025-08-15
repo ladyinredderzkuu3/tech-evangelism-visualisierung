bd0028e3e368638c7efe463092e2faae47061665
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-08-15 05:36:45 -0500

    feat: initial commit for src/TechnologyVisualizer.js

---
+++ src/TechnologyVisualizer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TechnologyVisualizer() {
+  return (
+    <div>
+      <h2>Wählen Sie eine Technologie aus</h2>
+      {/* Hier kann die Logik für die interaktive Visualisierung implementiert werden */}
+    </div>
+  );
+}
+
+export default TechnologyVisualizer;
